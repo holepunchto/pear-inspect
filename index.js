b0e4d4a5197d4168abeb3a85159385e72499b331
Tobias Baunbaek <freeall@gmail.com>
2024-03-12 09:49:34 +0100

    Less guarding

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class Inspector {
       }
     }
 
-    const pearFilename = global.Pear && path.join(global.Pear.config?.dir, global.Pear.config?.main)
+    const pearFilename = global.Pear && path.join(global.Pear.config.dir, global.Pear.config.main)
     this.filename = filename || pearFilename || require?.main?.filename || process?.argv?.[1]
     this.inspector = inspector
     this.dhtServer = dhtServer || null
